448ebe199cc666ac346d1f6fe415f0b01d5518ad
diegolloz98 <is714905@iteso.mx>
2022-02-22 20:17:37 -0600

    vvv

--- lib/user_page/profile.dart
+++ lib/user_page/profile.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:share/share.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'bloc/picture_bloc.dart';
 import 'circular_button.dart';
@@ -13,6 +17,7 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  ScreenshotController _screenshotController = ScreenshotController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,88 +26,100 @@ class _ProfileState extends State<Profile> {
           IconButton(
             tooltip: "Compartir pantalla",
             onPressed: () {
-              // TODO
+              _tomarScreenshotyCompartir();
+              print("take screen");
             },
             icon: Icon(Icons.share),
           ),
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(24.0),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              BlocConsumer<PictureBloc, PictureState>(
-                listener: (context, state) {
-                  if (state is PictureErrorState) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text("${state.errorMsg}")),
-                    );
-                  }
-                },
-                builder: (context, state) {
-                  if (state is PictureSelectedState) {
-                    return CircleAvatar(
-                      backgroundImage: FileImage(state.picture!),
-                      minRadius: 40,
-                      maxRadius: 80,
-                    );
-                  } else {
-                    return CircleAvatar(
-                      backgroundColor: Color.fromARGB(255, 122, 113, 113),
-                      minRadius: 40,
-                      maxRadius: 80,
-                    );
-                  }
-                },
-              ),
-              SizedBox(height: 16),
-              Text(
-                "Bienvenido",
-                style: Theme.of(context)
-                    .textTheme
-                    .headline4!
-                    .copyWith(color: Colors.black),
-              ),
-              SizedBox(height: 8),
-              Text("Usuario${UniqueKey()}"),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  CircularButton(
-                    textAction: "Ver tarjeta",
-                    iconData: Icons.credit_card,
-                    bgColor: Color(0xff123b5e),
-                    action: null,
-                  ),
-                  CircularButton(
-                    textAction: "Cambiar foto",
-                    iconData: Icons.camera_alt,
-                    bgColor: Colors.orange,
-                    action: () {
-                      BlocProvider.of<PictureBloc>(context).add(
-                        ChangeImageEvent(),
+      body: Screenshot(
+        controller: _screenshotController,
+        child: Padding(
+          padding: EdgeInsets.all(24.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                BlocConsumer<PictureBloc, PictureState>(
+                  listener: (context, state) {
+                    if (state is PictureErrorState) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("${state.errorMsg}")),
                       );
-                    },
-                  ),
-                  CircularButton(
-                    textAction: "Ver tutorial",
-                    iconData: Icons.play_arrow,
-                    bgColor: Colors.green,
-                    action: null,
-                  ),
-                ],
-              ),
-              SizedBox(height: 48),
-              CuentaItem(),
-              CuentaItem(),
-              CuentaItem(),
-            ],
+                    }
+                  },
+                  builder: (context, state) {
+                    if (state is PictureSelectedState) {
+                      return CircleAvatar(
+                        backgroundImage: FileImage(state.picture!),
+                        minRadius: 40,
+                        maxRadius: 80,
+                      );
+                    } else {
+                      return CircleAvatar(
+                        backgroundColor: Color.fromARGB(255, 122, 113, 113),
+                        minRadius: 40,
+                        maxRadius: 80,
+                      );
+                    }
+                  },
+                ),
+                SizedBox(height: 16),
+                Text(
+                  "Bienvenido",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline4!
+                      .copyWith(color: Colors.black),
+                ),
+                SizedBox(height: 8),
+                Text("Usuario${UniqueKey()}"),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    CircularButton(
+                      textAction: "Ver tarjeta",
+                      iconData: Icons.credit_card,
+                      bgColor: Color(0xff123b5e),
+                      action: null,
+                    ),
+                    CircularButton(
+                      textAction: "Cambiar foto",
+                      iconData: Icons.camera_alt,
+                      bgColor: Colors.orange,
+                      action: () {
+                        BlocProvider.of<PictureBloc>(context).add(
+                          ChangeImageEvent(),
+                        );
+                      },
+                    ),
+                    CircularButton(
+                      textAction: "Ver tutorial",
+                      iconData: Icons.play_arrow,
+                      bgColor: Colors.green,
+                      action: null,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 48),
+                CuentaItem(),
+                CuentaItem(),
+                CuentaItem(),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
+  
+  void _tomarScreenshotyCompartir() async {
+	final uint8List = await _screenshotController.capture();
+	String tempPath = (await getTemporaryDirectory()).path;
+	File file = File('$tempPath/image.png');
+	await file.writeAsBytes(uint8List!);
+	await Share.shareFiles([file.path]);
+}
 }
